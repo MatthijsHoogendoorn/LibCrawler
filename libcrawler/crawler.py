8e9ce35ec2545354f90b5f3eeaa152b370f0c58d
MatthijsHoogendoorn <matthijshoogendoorn@gmail.com>
2017-12-28 18:12:41 +0100

    Added new line for readability.

--- libcrawler/crawler.py
+++ libcrawler/crawler.py
@@ -68,6 +68,7 @@ class Crawler:
 
                                 if targetfound:
                                     break
+
                             node = parent
                             parent = node.getparent()
 
